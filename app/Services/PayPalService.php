c4327ca82cc75f214eb2dbf2e32b0e8c54446d7c
AlejoGomez1702 <luis.gomezc@autonoma.edu.co>
2020-06-11 13:27:08 -0500

    Migraciones y seeders de procesadores de pago

---
+++ app/Services/PayPalService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use App\Traits\ConsumesExternalServices;
+
+class PayPalService
+{
+    use ConsumesExternalServices;
+
+    protected $baseUri;
+
+    protected $clientId;
+
+    protected $clientSecret;
+
+    public function __construct()
+    {
+        $this->baseUri = config('services.paypal.base_uri');
+        $this->clientId = config('services.paypal.client_id');
+        $this->clientSecret = config('services.paypal.client_secret');
+    }
+
+    public function resolveAuthorization(&$queryParams, &$formParams, &$headers)
+    {
+        $headers['Content-Type'] = 'application/json';
+        $headers['Authorization'] = $this->resolveAccessToken();
+    }
+
+    public function decodeResponse($response)
+    {
+        return json_decode($response);
+    }
+
+    public function resolveAccessToken()
+    {
+        $credentials = base64_encode($this->clientId . ":" . $this->clientSecret);
+        echo($credentials);
+        var_dump($credentials);
+
+        return "Bearer {$credentials}";
+    }
+}
